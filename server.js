70de0898f1524613525648bb83d417aae5e09a11
marina <golovina.marina@gmail.com>
2021-06-03 16:56:51 +1000

    Working Hello World

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+const router = require('./routes')
+const server = express()
+
+// Server configuration
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: false }))
+server.use(express.json())
+
+// Handlebars configuration
+server.engine('hbs', hbs({ extname: 'hbs' }))
+server.set('view engine', 'hbs')
+
+// Your routes/router(s) should go here
+
+server.use('/', router)
+
+
+module.exports = server
