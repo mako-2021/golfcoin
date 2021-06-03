eef20720f916b92f09a806548b0a8cafbf363107
marina <golovina.marina@gmail.com>
2021-06-03 20:59:59 +1000

    Update file system

--- routes.js
+++ routes.js
@@ -4,9 +4,27 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
     const viewData = {
-        text: 'Hello from home'
+        title: 'Choose your Story:',
+        text: "Story Option",
+        footer: "FOOTER"
+
     }
     res.render('home', viewData);
 });
+
+
+// Jest installed - ready for testing
+// Repo deployed on Heroku
+// CSS file linked - ready for styling
+// data.json created to store stories data, etc..
+
+// Links to story pages:
+// router.get('/story/:id', (req, res) => {
+//     res.render('storyOne', viewDataStoryOne)
+//     res.render('storyTwo', viewDataStoryTwo)
+//     res.render('storyThree', viewDataStoryThree)
+// })
+
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ server.use(express.urlencoded({ extended: false }))
 server.use(express.json())
 
 // Handlebars configuration
+server.use(express.static('public'))
 server.engine('hbs', hbs({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 
